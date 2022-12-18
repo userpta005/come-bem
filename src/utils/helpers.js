c9a485d417892e8afa20b92ce076294dd908f4ce
Rafael Arend <rafinhaarend123@hotmail.com>
2022-12-17 23:09:18 -0400

    corrige função brDate

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,16 +1,13 @@
+import { parseISO, format } from 'date-fns'
+
 const floatToMoney = (currency) => {
   const formatted = parseInt(currency).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
   return formatted
 }
 
 const brDate = (timestamp) => {
-  const date = new Date(timestamp)
-
-  return date.toLocaleString('pt-BR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit'
-  })
+  const date = parseISO(timestamp)
+  return format(date, 'dd/MM/yyyy')
 }
 
 export {
