e4a38d39c7bc0e2e490284943727f05464a434e5
Rafael Arend <rafinhaarend123@hotmail.com>
2022-12-20 12:47:01 -0400

    Insere QCalendar pra teste

--- src/pages/app/dependents/View.vue
+++ src/pages/app/dependents/View.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-md-9 col-xs-12 self-start q-pa-sm">
         <DependentHeader />
+        <QCalendar />
       </div>
       <SidebarActions class="col-md-3 col-xs-12 self-center"
         :class="{ 'order-first': $q.screen.lt.md }" />
@@ -15,12 +16,14 @@ import { defineComponent } from 'vue'
 import DependentHeader from 'src/components/app/DependentHeader.vue'
 import SidebarActions from 'src/components/app/SidebarActions.vue'
 import useStorageStore from 'src/stores/storage'
+import QCalendar from 'src/components/app/QCalendar.vue'
 
 export default defineComponent({
   name: 'DependentViewPage',
   components: {
     DependentHeader,
-    SidebarActions
+    SidebarActions,
+    QCalendar
   },
   setup () {
     const store = useStorageStore()
